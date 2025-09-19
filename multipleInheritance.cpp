2eb7329824c3137ac8e4f1754a11792a253d8a4d
Zukliod <156461915+Zukliod@users.noreply.github.com>
2025-09-19 21:16:36 +0530

    multipleInheritance program

---
+++ multipleInheritance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class A {
+    public :
+    void displayA() {
+        cout << "This is class A" << endl;
+    }
+};
+class B {
+    public :
+    void displayB() {
+        cout << "This is class B" << endl;
+    }
+};
+class C : public A, public B {
+    public :
+    void displayC() {
+        cout << "This is class C" << endl;
+    }
+};
+int main() {
+    C obj;
+    obj.displayA();
+    obj.displayB();
+    obj.displayC();
+    return 0;
+}
